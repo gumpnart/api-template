ce4b90dba045741917dea168419a5df91d1bd577
Gumpnart Kengpornsakul <armie.gumpnart@outlook.com>
2020-12-06 23:59:23 +0700

    clean repo by remove modules except UserModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { ArticleModule } from './article/article.module';
 import { UserModule } from './user/user.module';
-import { ProfileModule } from './profile/profile.module';
-import { TagModule } from './tag/tag.module';
 
 @Module({
-  imports: [
-    ArticleModule,
-    UserModule,
-    ProfileModule,
-    TagModule
-  ],
-  controllers: [
-    AppController
-  ],
-  providers: []
+  imports: [UserModule],
+  controllers: [AppController],
+  providers: [],
 })
 export class ApplicationModule {}
